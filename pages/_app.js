9670524c51ad806589cf62cbc547211dc9eff5fa
Max Leiter <mleiter@usc.edu>
2020-12-02 21:04:51 -0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import { SWRConfig } from 'swr'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <SWRConfig value={{ refreshInterval: process.env.VERCEL_URL ? 10000 : 60000 }}><Component {...pageProps} /></SWRConfig>
 }
 
 export default MyApp
